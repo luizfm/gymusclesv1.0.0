240dcbcf36470980ce23b3018bd5512d34e71c1c
Luiz Fernando de Morais <luiz.ado@hotmail.com>
2020-06-14 20:38:31 -0300

    Added delete and update member's SQL. Projet finished

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,28 +1,70 @@
+const { age, date } = require('../lib/utils')
+
 const Member = require('../controllers/models/Member')
+
+function fillAllTheFields(keys, data) {
+  for (key of keys) {
+    if(data[key] == "") {
+      return res.render('Please, fill all the fields')
+    }
+  }
+}
 
 module.exports = {
   index(req, res) {
     Member.all(function(members) {
+
+
       return res.render('members/index', { members })
     })
     
   },
   create(req, res) {
-
+    return res.render('members/create')
   },
   post(req, res) {
 
+    const keys = Object.keys(req.body)
+
+    fillAllTheFields(keys, req.body)
+
+    Member.create(req.body, function(member) {
+      return res.redirect(`members/${member.id}`)
+    })
+
   },
   show(req, res) {
+    Member.find(req.params.id, function(member) {
 
+
+      member.birth = age(member.birth)
+      member.services = member.services.split(',')
+      
+      return res.render('members/show', { member })
+    })
+    
   },
   edit(req, res) {
+    Member.find(req.params.id, function(member) {
+      if(!member) return res.send('Member not found!')
 
+      member.birth = date(member.birth).iso
+
+      return res.render('members/edit', { member })
+    })
   },
   update(req, res) {
+    const keys = Object.keys(req.body)
 
+    fillAllTheFields(keys, req.body)
+
+    Member.update(req.body, function() {
+      return res.redirect(`members/${req.body.id}`)
+    })
   },
   delete(req, res) {
-
+    Member.delete(req.body.id, function() {
+      return res.redirect('/members')
+    })
   }
 }
--- src/app/controllers/models/Member.js
+++ src/app/controllers/models/Member.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const { date, age } = require('../../lib/utils')
 
 module.exports = {
   all(callback) {
@@ -13,5 +14,80 @@ module.exports = {
     } catch(err) {
       console.error(err)
     }
+  },
+  create(data, callback) {
+    const query = `
+      INSERT INTO members (
+        name,
+        height,
+        weight,
+        gender,
+        birth,
+        services,
+        avatar_url
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING id
+    `
+
+    const values = [
+      data.name,
+      data.height,
+      data.weight,
+      data.gender,
+      date(data.birth).iso,
+      data.services,
+      data.avatar_url
+    ]
+
+    db.query(query, values, function(err, results) {
+      if(err) throw 'Database error'
+
+      callback(results.rows[0])
+    })
+  },
+  find(id, callback) {
+    db.query(`SELECT * FROM members WHERE id = $1`, [id], function(err, results) {
+      if(err) throw 'Database error'
+
+      callback(results.rows[0])
+    })
+  },
+  update(data, callback) {
+    const query = `
+      UPDATE members SET
+      name = ($1),
+      height = ($2),
+      weight = ($3),
+      gender = ($4),
+      birth = ($5),
+      services = ($6),
+      avatar_url = ($7)
+      WHERE id = $8
+    `
+
+    const values = [
+      data.name,
+      data.height,
+      data.weight,
+      data.gender,
+      date(data.birth).iso,
+      data.services,
+      data.avatar_url,
+      data.id
+    ]
+
+    db.query(query, values, function(err, results) {
+      if(err) throw 'Database error'
+
+      callback(results.rows[0])
+    })
+
+  },
+  delete(id, callback) {
+    db.query(`DELETE FROM members WHERE id = $1`, [id], function(err, results) {
+      if(err) throw 'Instructor not found'
+
+      callback();
+    })
   }
 }
